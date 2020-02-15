60f529e169c9467a126aee32f23426377f24a5ed
flowersfaerie <flowersfaerie@gmail.com>
2020-02-15 09:19:19 -0800

    modified 1 link

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import {Route} from 'react-router-dom';
 const App = () => (
   <div>
     <Navigation />
-    <Route exact path="/" component={Home} />
+    <Route path="/home" component={Home} />
     <Route path="/about" component={About} />
     <Route path="/contact" component={Contact} />
   </div>
